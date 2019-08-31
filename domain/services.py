fdfab481cde215fd4b05bda6a43561a601def650
Alex L. Arias <aleksarias@gmail.com>
2019-08-31 15:29:30 -0400

    fourth hour commit

---
+++ domain/services.py
@@ -0,0 +1,19 @@
+from domain.repositories import VehicleRepository, LocationMapRepository, PeopleRepository
+
+
+class PersonService(object):
+
+    def __init__(self, repo: PeopleRepository):
+        pass
+
+
+class VehicleService(object):
+
+    def __init__(self, repo: VehicleRepository):
+        pass
+
+
+class LocationMapService(object):
+
+    def __init__(self, repo: LocationMapRepository):
+        pass
---
+++ infrastructure/persistence/inmem.py
@@ -0,0 +1,11 @@
+from domain.repositories import PeopleRepository, VehicleRepository, LocationMapRepository
+
+
+class InMemPeopleRepo(PeopleRepository):
+    pass
+
+class InMemVehicleRepository(VehicleRepository):
+    pass
+
+class InMemLocationMapRepository(LocationMapRepository):
+    pass
